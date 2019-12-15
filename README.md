74de5c353aba2215312c092461eb546e17468ab8
manish <k88.manish@gmail.com>
2019-12-15 23:02:01 +0530

    Added transformation API

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ In order to run the application:
 1. Install dependencies by executing: `npm install` or `yarn`.
 2. Start the server by executing `node index.js`
 3. Navigate to http://localhost:8000 on your browser.
+
+#API to get gists information for the user
+
+- http://localhost:8000/gists/:username
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 // Import the axios library, to make HTTP requests
 const axios = require("axios");
+const https = require("https");
+const request = require("request");
+const fs = require("fs");
 const GIT_CONSTS = require("./constants/github");
 
 const JWT_SECRET = "abctestappxyz";
@@ -47,7 +50,6 @@ app.get("/github/oauth/callback", (req, res) => {
     res.redirect(`/welcome.html`);
   });
 });
-
 app.get("/user", (req, res) => {
   const token = req.cookies.jwt;
   if (!token) {
@@ -69,6 +71,64 @@ app.get("/user", (req, res) => {
   } catch {
     return res.status(401).send("Invalid token");
   }
+});
+
+/**
+ * Helper function to get the content of the file
+ * @param {*} url
+ */
+const getFileContent = async function(url) {
+  return new Promise(resolve => {
+    request.get(url, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        resolve(body);
+      }
+    });
+  });
+};
+
+/**
+ * Helper function to loop in async await manner
+ * @param {*} array
+ * @param {*} callback
+ */
+const asyncForEach = async function(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+};
+
+app.get("/gists/:username", (req, res) => {
+  const { username } = req.params;
+
+  if (!username) {
+    return res.status(400).send("Username is required");
+  }
+
+  axios({
+    method: "get",
+    url: `https://api.github.com/users/${username}/gists`
+  }).then(async response => {
+    const { data } = response;
+    const transformedData = [];
+    await asyncForEach(data, async gistInfo => {
+      try {
+        const info = { id: gistInfo.id, owner: gistInfo.owner.login, owner_id: gistInfo.owner.id, description: gistInfo.description, files: [] };
+
+        for (let file in gistInfo.files) {
+          const fileInfo = gistInfo.files[file];
+          const fileContent = await getFileContent(fileInfo.raw_url);
+          info.files.push({ name: fileInfo.filename, language: fileInfo.language, content: fileContent });
+        }
+
+        transformedData.push(info);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    res.status(200).send(transformedData);
+  });
 });
 
 // Start the server on port 8080
